cf5e534a05fd091a9b4d9bb1b87beab094bc7e9b
katayama8000 <tattu.0310@gmail.com>
2023-02-04 16:36:44 +0900

    jotai

--- doctormate-tech/pages/index9.tsx
+++ doctormate-tech/pages/index9.tsx
@@ -2,17 +2,28 @@ import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword, User } from "firebase/auth";
 import { Box, Button, Input } from "@chakra-ui/react";
 import { auth } from "../firebase/firebase";
+import { useAtomValue, useSetAtom } from "jotai";
+import { getUserEmailAtom, setUserAtom } from "../state/user.state";
 
 const Index9 = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const setUser = useSetAtom(setUserAtom);
+  const userEmail = useAtomValue(getUserEmailAtom);
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user: User = userCredential.user;
+        setUser(user);
         console.log(user);
-        // ...
+        toast({
+          title: "success",
+          description: "Document successfully written!",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -35,8 +46,20 @@ const Index9 = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </Box>
+      <div>
+        <h1>mail: {userEmail && userEmail}</h1>
+      </div>
     </div>
   );
 };
 
 export default Index9;
+function toast(arg0: {
+  title: string;
+  description: string;
+  status: string;
+  duration: number;
+  isClosable: boolean;
+}) {
+  throw new Error("Function not implemented.");
+}
--- doctormate-tech/state/user.state.ts
+++ doctormate-tech/state/user.state.ts
@@ -3,9 +3,9 @@ import { atom } from "jotai";
 
 export const userAtom = atom<User | null>(null);
 export const getUserAtom = atom<User | null>((get) => get(userAtom));
-export const setUserAtom = atom<null, User | null>(
-  null,
-  (_get, set, update) => {
-    set(userAtom, update);
-  }
+export const setUserAtom = atom<null, User>(null, (_get, set, update) => {
+  set(userAtom, update);
+});
+export const getUserEmailAtom = atom<string | null | undefined>(
+  (get) => get(userAtom)?.email
 );
