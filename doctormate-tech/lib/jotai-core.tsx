def5d21e8e6686143855deadb4d3d44e1855723a
katayama8000 <tattu.0310@gmail.com>
2023-02-11 13:17:56 +0900

    wip

--- doctormate-tech/lib/jotai-core.tsx
+++ doctormate-tech/lib/jotai-core.tsx
@@ -5,7 +5,7 @@ type TUser = {
   age: number;
 };
 
-export const atom = (initialValue: TUser) => ({
+export const atom = (initialValue: TUser): { init: TUser } => ({
   init: initialValue,
 });
 
@@ -13,29 +13,40 @@ const atomStateMap = new WeakMap<
   { init: TUser },
   { value: TUser; listeners: Set<() => void> }
 >();
-export const getAtomState = (atom: { init: TUser }) => {
+
+export const getAtomState = (atom: {
+  init: TUser;
+}): { value: TUser; listeners: Set<() => void> } => {
   let atomState = atomStateMap.get(atom);
+  console.log('atomStateMap', atomState);
   if (!atomState) {
+    console.log('atomStateMap', !!atomState);
     atomState = { value: atom.init, listeners: new Set<() => void>() };
     atomStateMap.set(atom, atomState);
   }
+  console.log('atomState', atomState);
   return atomState;
 };
 
 export const useAtom = (atom: { init: TUser }) => {
   const atomState = getAtomState(atom);
+  console.log('atomStateMap', atomState);
   const [value, setValue] = useState<TUser>(atomState.value);
   useEffect(() => {
     const callback = () => setValue(atomState.value);
     atomState.listeners.add(callback);
     callback();
+    console.log(atomState);
     return () => {
       atomState.listeners.delete(callback);
     };
   }, [atomState]);
   const setAtom = (nextValue: TUser) => {
     atomState.value = nextValue;
-    atomState.listeners.forEach((l: () => void) => l());
+    atomState.listeners.forEach((l: () => void) => {
+      l();
+      console.log('llllll', l);
+    });
   };
   return [value, setAtom] as const;
 };
