96299317556cc4def11c4bcfb28691ea3893b579
katayama8000 <tattu.0310@gmail.com>
2023-02-25 12:49:49 +0900

    zod

--- d-tech/pages/index16.tsx
+++ d-tech/pages/index16.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import { Button } from '@chakra-ui/react';
 
 const schema = z.object({
   cost: z.string().transform((val) => Number(val)),
+  test: z.string(),
 });
 
 type Schema = z.infer<typeof schema>;
@@ -12,6 +14,7 @@ type Schema = z.infer<typeof schema>;
 const COSTS = [1000, 2000, 3000, 5000, 10000] as const;
 const Form = () => {
   const {
+    control,
     watch,
     register,
     handleSubmit,
@@ -26,6 +29,9 @@ const Form = () => {
   console.log(watch('cost'));
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <Button onClick={() => console.log({ ...register('cost') })}>
+        click
+      </Button>
       <select {...register('cost')}>
         {COSTS.map((cost) => (
           <option key={cost} value={cost}>
@@ -33,6 +39,18 @@ const Form = () => {
           </option>
         ))}
       </select>
+      <Controller
+        name="test"
+        control={control}
+        render={({ field }) => {
+          return (
+            <>
+              <Button onClick={() => console.log({ ...field })}>click</Button>
+              <input {...field} />
+            </>
+          );
+        }}
+      />
       <button type="submit">送信</button>
     </form>
   );
