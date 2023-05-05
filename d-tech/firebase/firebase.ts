ba6d8debfa2c13af602ca98ec215fbb7f741e090
katayama8000 <tattu.0310@gmail.com>
2023-05-05 19:06:33 +0900

    update

--- d-tech/firebase/firebase.ts
+++ d-tech/firebase/firebase.ts
@@ -16,4 +16,4 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
