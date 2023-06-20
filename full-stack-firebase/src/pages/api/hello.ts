2fe50ec7d30b72bdcdff1bca5428adc49e88be98
katayama8000 <tattu.0310@gmail.com>
2023-06-20 22:32:15 +0900

    admin

--- full-stack-firebase/src/pages/api/hello.ts
+++ full-stack-firebase/src/pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import * as admin from 'firebase-admin';
+const serviceAccount = require('key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const db = admin.firestore();
+  // write
+  db.collection('users').add({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815,
+  });
+  res.status(200).json({ name: 'John Doe' });
 }
